c5642ede7f7082b14d43d8eac3858e35d5ed955a
Auralevant <animegamernazir@gmail.com>
2026-09-11 17:56:43 -0400

    Update AuraBot.py

--- AuraBot.py
+++ AuraBot.py
@@ -9,7 +9,6 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True  # needed for prefix commands to work
-
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 async def load_cogs():
